add verbose mode that helped me find that off-by-one bug :)

vlfeat/dsift.py
@@ -104,7 +104,8 @@ def vl_dsift_transpose_descriptor(dest, src, num_bin_t, num_bin_x, num_bin_y):
 
 
 def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
-              window_size=None, float_descriptors=False, matlab_style=True):
+              window_size=None, float_descriptors=False,
+              verbose=False, matlab_style=True):
     '''
     Dense sift descriptors from an image.
 
@@ -152,6 +153,28 @@ def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
         # get calculated parameters
         descr_size = dsift.descrSize
         num_frames = dsift.numFrames
+        geom = dsift.geom
+
+        if verbose:
+            pr = lambda *a, **k: print('vl_dsift:', *a, **k)
+            pr("image size         [W, H] = [{}, {}]".format(N, M))
+            x0 = dsift.boundMinX + 1
+            y0 = dsift.boundMinY + 1
+            x1 = dsift.boundMaxX + 1
+            y1 = dsift.boundMaxY + 1
+            bound_args = [y0, x0, y1, x1] if matlab_style else [x0, y0, x1, y1]
+            pr("bounds:            [minX,minY,maxX,maxY] = [{}, {}, {}, {}]"
+                .format(*bound_args))
+            pr("subsampling steps: stepX={}, stepY={}".format(
+                    dsift.stepX, dsift.stepY))
+            pr("num bins:          [numBinT, numBinX, numBinY] = [{}, {}, {}]"
+                .format(geom.numBinT, geom.numBinX, geom.numBinY))
+            pr("descriptor size:   {}".format(descr_size))
+            pr("bin sizes:         [binSizeX, binSizeY] = [{}, {}]".format(
+                    geom.binSizeX, geom.binSizeY))
+            pr("flat window:       {}".format(bool(fast)))
+            pr("window size:       {}".format(dsift.windowSize))
+            pr("num of features:   {}".format(num_frames))
 
         # do the actual processing
         vl_dsift_process(dsift_p, data)
@@ -177,7 +200,6 @@ def vl_dsift(data, fast=False, norm=False, bounds=None, size=3, step=1,
             descrs = descrs.astype(np.uint8)  # TODO: smarter about copying?
         if matlab_style:
             new_order = np.empty(descr_size, dtype=int)
-            geom = dsift.geom
             vl_dsift_transpose_descriptor(new_order, np.arange(descr_size),
                 geom.numBinT, geom.numBinX, geom.numBinY)
             descrs = descrs[:, new_order]

vlfeat/imsmooth.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 
 import numpy as np
 from numpy.ctypeslib import ndpointer
@@ -27,7 +27,7 @@ PADDINGS = {'continuity': 1, 'zero': 0}
 TRANSPOSE = 4
 
 
-def vl_imsmooth(image, sigma, step=1, padding='continuity'):
+def vl_imsmooth(image, sigma, step=1, padding='continuity', verbose=False):
     # unlike matlab interface, only does gaussian kernel
 
     if not np.isscalar(sigma):
@@ -69,6 +69,16 @@ def vl_imsmooth(image, sigma, step=1, padding='continuity'):
     out_rows, out_cols = (np.asarray(image.shape[:2]) - 1) // step + 1
 
     output = np.zeros((out_rows, out_cols, channels), dtype=dtype, order='F')
+
+    if verbose:
+        pr = lambda *a, **k: print('vl_imsmooth:', *a, **k)
+        pr('[{}x{}x{}] -> [{}x{}x{}] ({}, subsampling step {})'.format(
+            in_rows, in_cols, channels, out_rows, out_cols, channels,
+            dtype, step))
+        pr('padding: {}'.format(padding))
+        pr('kernel: gaussian')
+        pr('sigma: {}'.format(sigma))
+        # TODO: SIMD enabled?
 
     # obtained from the input image by convolving and downsampling along the
     # height, saving the result transposed

vlfeat/phow.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 import numpy as np
 
 from .imsmooth import vl_imsmooth
@@ -11,7 +11,7 @@ DEFAULT_SIZES = (4, 6, 8, 10)
 
 def vl_phow(image, sizes=DEFAULT_SIZES, fast=True, step=2, color='gray',
             contrast_thresh=0.005, window_size=1.5, magnif=6,
-            float_descriptors=False):
+            float_descriptors=False, verbose=False):
 
     if color not in COLOR_CHOICES:
         raise ValueError("unknown color {!r}; expected one of {}".format(
@@ -23,6 +23,7 @@ def vl_phow(image, sizes=DEFAULT_SIZES, fast=True, step=2, color='gray',
         'fast': fast,
         'float_descriptors': float_descriptors,
         'step': step,
+        'verbose': verbose,
         'matlab_style': True,
     }
 
@@ -61,6 +62,12 @@ def vl_phow(image, sizes=DEFAULT_SIZES, fast=True, step=2, color='gray',
                 (r + g - 2 * b) / np.sqrt(2) + alpha * mu,
             ])
             del r, g, b, mu, alpha
+
+    if verbose:
+        pr = lambda *a, **k: print('vl_phow:', *a, **k)
+        pr('color space: {}'.format(color))
+        pr('image size: {} x {}'.format(*image.shape[:2]))
+        pr('sizes: [{}]'.format(', '.join(map(str, sizes))))
 
     frames = []
     descrs = []