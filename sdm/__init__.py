35b18e1aa046fd8d574d083e3cb590dbaab323f0
Dougal Sutherland <dougal@gmail.com>
2013-03-28 18:38:45 -0400

    import read/save features funcs into top-level namespace

--- sdm/__init__.py
+++ sdm/__init__.py
@@ -2,6 +2,8 @@ from . import utils
 from . import mp_utils
 
 from . import image_features
+from .image_features import (read_features, save_features,
+                             read_features_perimage, save_features_perimage)
 
 from . import extract_image_features
 from .extract_image_features import extract_features, get_features
