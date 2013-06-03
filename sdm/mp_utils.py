4dbeffc97c8180edc249de2060db5967b9c8dd31
Dougal Sutherland <dougal@gmail.com>
2013-06-03 15:18:30 -0400

    move the ForkedData OS check to be on pickling
    In theory, this lets you do single-threaded SDMs on Windows. Who
    knows if that actually works or not.

--- sdm/mp_utils.py
+++ sdm/mp_utils.py
@@ -115,12 +115,15 @@ class ForkedData(object):
     '''
     # TODO: more flexible garbage collection options
     def __init__(self, val):
-        if os.name != 'posix':
-            raise RuntimeError("ForkedData only works on OSes with fork()")
         g = globals()
         self.name = next(n for n in _data_name_cands if n not in g)
         g[self.name] = val
         self.master_pid = os.getpid()
+
+    def __getstate__(self):
+        if os.name != 'posix':
+            raise RuntimeError("ForkedData only works on OSes with fork()")
+        return self.__dict__
 
     @property
     def value(self):
