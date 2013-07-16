4bf70acf3ee77d6b0a0e64bce44a383e97622ed2
Dougal Sutherland <dougal@gmail.com>
2013-07-16 02:18:52 -0400

    Actually .seekable() isn't there for everything in 2.x.
    This reverts commit b0c7810c31731f1ea66334c8db40cd5b5312d921.

--- sdm/typedbytes_utils.py
+++ sdm/typedbytes_utils.py
@@ -34,12 +34,30 @@ def _npy_size(ary):
     return magic_len + header_len + data_len
 
 
-def read_ndarray(f):
+def _file_is_seekable(f):
+    try:
+        f.tell()
+    except IOError as e:
+        if e.errno == errno.ESPIPE and e.strerror == 'Illegal seek':
+            return False
+        raise
+    else:
+        return True
+
+def check_seekable(the_obj):
+    if not hasattr(the_obj, '_file_seekable'):
+        the_obj._file_seekable = _file_is_seekable(the_obj.file)
+
+
+def read_ndarray(f, file_is_seekable=None):
     length, = struct.unpack('>i', f.read(4))
     if length < 0:
         raise ValueError(r"bad length value {}".format(length))
 
-    if f.seekable():
+    if file_is_seekable is None:
+        file_is_seekable = _file_is_seekable(f)
+
+    if file_is_seekable:
         return np.load(f)
     else:
         with closing(StringIO()) as sio:
@@ -48,9 +66,11 @@ def read_ndarray(f):
             return np.load(sio)
 
 def _read_ndarray_in(self):
-    return read_ndarray(self.file)
+    seekable = getattr(self, '_file_seekable', None)
+    return read_ndarray(self.file, file_is_seekable=seekable)
 
 def register_read_ndarray(input_object):
+    check_seekable(input_object)
     input_object.register(NUMPY_CODE, _read_ndarray_in)
 
 
@@ -77,7 +97,6 @@ def register_np_writes(output_object):
     r(np.int64, 'write_long')
     r(np.float32, 'write_float')
     r(np.float64, 'write_double')
-    r(np.string_, 'write_string')
 
 
 ################################################################################
